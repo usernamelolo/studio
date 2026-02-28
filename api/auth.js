7a1a9e4e3a7e15015397bc39a42deba8364d2008
usernamelolo <142486246+usernamelolo@users.noreply.github.com>
2026-02-28 21:13:47 +0300

    auth.js

--- api/auth.js
+++ api/auth.js
@@ -6,10 +6,18 @@ module.exports = async (req, res) => {
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   if (req.method === 'POST') {
-    const { password } = req.body || {};
-    if (password === process.env.SITE_PASSWORD) {
+    let body = {};
+    try {
+      body = typeof req.body === 'string' ? JSON.parse(req.body) : req.body || {};
+    } catch (e) {}
+
+    const { username, password } = body;
+
+    if (username === process.env.SITE_USERNAME && password === process.env.SITE_PASSWORD) {
       return res.status(200).json({ success: true });
     }
     return res.status(401).json({ success: false });
   }
+
   res.status(405).json({ success: false });
+};
