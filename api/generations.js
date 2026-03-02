4f6b4422d1d93a3d3a3f85c9c8d2e1e2e56fbf24
usernamelolo <142486246+usernamelolo@users.noreply.github.com>
2026-03-02 23:45:13 +0300

    generations.js

--- api/generations.js
+++ api/generations.js
@@ -1,4 +1,4 @@
-xport default async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
